8e2d28d5f08b4baacd307047e91398b93b76d6ca
Yasin <yasinerdemli35@icloud.com>
2022-05-05 15:33:33 +0300

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Yasin Erdemli on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
